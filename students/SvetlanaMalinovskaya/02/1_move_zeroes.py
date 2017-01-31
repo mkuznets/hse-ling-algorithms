6fdbf62e975e8ab3c6f404385e35abd586e69256
Svetlana Malinovskaya <soerty97@gmail.com>
2017-02-01 00:17:07 +0300

    Again fix

--- students/SvetlanaMalinovskaya/02/1_move_zeroes.py
+++ students/SvetlanaMalinovskaya/02/1_move_zeroes.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        zeroes = []
         length = len(nums)
-
-        for index in range(length):
-            if nums[index] == 0:
-                zeroes.append(index)
-
         last = length-1
-        while zeroes:
-            index = zeroes.pop()
-            nums[index], nums[last] = nums[last], nums[index]
-            last -= 1
+
+        for index in range(last, 0, -1):
+            if nums[index] == 0:
+                nums[index], nums[last] = nums[last], nums[index]
+                last -= 1
