7324f89a5dd8c6e1ba2c6afd8aad641e526d71ae
Svetlana Malinovskaya <soerty97@gmail.com>
2017-02-01 00:36:24 +0300

    New solution

--- students/SvetlanaMalinovskaya/02/4_detect_cycle.py
+++ students/SvetlanaMalinovskaya/02/4_detect_cycle.py
@@ -1,20 +1,23 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+
 
 class Solution(object):
     def hasCycle(self, head):
-        # Сложность O(n^2)
-        node = head
+        # Сложность O(n)
+        nodesSeen = {}
 
+        node = head
         while node:
+            if node in nodesSeen:
+                return True
+
+            nodesSeen[node] = True
             node = node.next
-            next = node
-            while next:
-                if next.next is node:
-                    return True
-                next = next.next
 
         return False
