9824497da5acd4bb9ae7da01fb17b64b91f5edf4
phuuda <phuuda@gmail.com>
2017-02-04 16:55:10 +0300

    Стырина Соня, БКЛ141, 02-3

---
+++ students/sofia styrina/task 2/styrina_alg2_3.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, x, next = None):
+        self.value = x
+        self.next = next
+
+lst = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+
+class Solution:
+    def reverseList(self, head):
+        head1 = None
+        while head:
+            x = head
+            head = x.next
+            x.next = head1
+            head1 = x
+        return head1
+
+print(Solution().reverseList(lst).value)
+
+
+
