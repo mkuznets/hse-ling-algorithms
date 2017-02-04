04b24b53b17dbd4bda2d68667280e7d842f682db
SoDipole <sarun4ik7@yandex.ru>
2017-02-04 19:41:27 +0300

    Update 1_move_zeroes.py

--- students/SoninaPolina/02/1_move_zeroes.py
+++ students/SoninaPolina/02/1_move_zeroes.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        for n in nums:
-            if n == 0:
-                nums.remove(0)
-                nums.append(0)
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
