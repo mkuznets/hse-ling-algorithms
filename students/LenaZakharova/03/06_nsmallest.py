96f2e3b9a5da4eb535008fe0ab7746ebef0e56b1
eszakharova <1583253@gmail.com>
2017-02-19 16:08:13 +0300

    p6_2

--- students/LenaZakharova/03/06_nsmallest.py
+++ students/LenaZakharova/03/06_nsmallest.py
@@ -12,7 +12,7 @@ def partition(A, lo, hi):
 
 
 def pre_sort(li, first, last, m):
-    if last - first >= m:
+    if last - first > m+1:
         pivot = partition(li, first, last)
         pre_sort(li, first, pivot - 1, m)
         pre_sort(li, pivot + 1, last, m)
@@ -26,9 +26,12 @@ def nsmallest(m, A):
     >>> nsmallest(3, [44, 64, 21, 86, 40, 46, 95])
     [21, 40, 44]
     """
-    return pre_sort(A, 0, len(A)-1, m)[:m]
+    if m == 1:
+        return [min(A)]
+    else:
+        return pre_sort(A, 0, len(A)-1, m)[:m]
 
-# print(nsmallest(4, [44, 64, 21, 86, 40, 46, 95]))
-# print(nsmallest(2, [1, 64, 86, 40, 46, 95]))
-# print(nsmallest(2, [1, 1, 1, 1, 46, 95]))
-# print(nsmallest(6, [44, 64, 21, 86, 40, 46, 95]))
+print(nsmallest(4, [44, 64, 21, 86, 40, 46, 95]))
+print(nsmallest(2, [1, 64, 86, 40, 46, 95]))
+print(nsmallest(2, [1, 1, 1, 1, 46, 95]))
+print(nsmallest(1, [44, 64, 21, 86, 40, 46, 95]))
