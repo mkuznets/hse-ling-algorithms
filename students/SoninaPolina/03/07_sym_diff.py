39ec2a8c39722fe5d445deca6ea06ac6b7da658d
Polina Sonina <sarun4ik7@yandex.ru>
2017-02-20 00:40:54 +0300

    Update 07_sym_diff.py

--- students/SoninaPolina/03/07_sym_diff.py
+++ students/SoninaPolina/03/07_sym_diff.py
@@ -1,14 +1,11 @@
 def sym_diff(a, b):
-    """
-    Возвращает симметрическую разность двух массивов.
-    Исходные массивы считать отсортированными
-    и не содержащими повторяющихся элементов.
-    >>> sym_diff([1, 2, 3, 4, 5], [2, 4, 10])
-    [1, 3, 5, 10]
-    """
-
     result = []
-
-    ...
-
+    for i in range(len(a)):
+        if a[i] not in b:
+            result.append(a[i])         
+    for i in range(len(b)):
+        if b[i] not in a:
+            result.append(b[i])      
     return result
+
+print(sym_diff([1, 2, 3, 4, 5], [2, 4, 10]))
