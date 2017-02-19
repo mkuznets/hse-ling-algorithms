ee9fadd6150045d9b83d506cc69d6754398945a0
Polina Sonina <sarun4ik7@yandex.ru>
2017-02-19 22:38:42 +0300

    Create 06_nsmallest.py

---
+++ students/SoninaPolina/03/06_nsmallest.py
@@ -0,0 +1,22 @@
+def partition(A, lo, hi):
+    pivot = A[hi]
+    i = lo
+
+    for j in range(lo, hi):
+        if A[j] <= pivot:
+            A[i], A[j] = A[j], A[i]
+            i += 1
+
+    A[i], A[hi] = A[hi], A[i]
+    return i
+
+
+def nsmallest(m, A):
+    """
+    Возвращает `m` наименьших элементов массива A.
+    Функция реализована на основе Quicksort.
+    >>> nsmallest(3, [44, 64, 21, 86, 40, 46, 95])
+    [21, 40, 44]
+    """
+
+    ...
