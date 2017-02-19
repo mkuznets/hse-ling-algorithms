cfb9a35cd405c871be91bb7e9bdeda110f1365b2
Polina Sonina <sarun4ik7@yandex.ru>
2017-02-20 00:09:14 +0300

    Update 06_nsmallest.py

--- students/SoninaPolina/03/06_nsmallest.py
+++ students/SoninaPolina/03/06_nsmallest.py
@@ -1,22 +1,21 @@
 def partition(A, lo, hi):
     pivot = A[hi]
     i = lo
-
     for j in range(lo, hi):
         if A[j] <= pivot:
             A[i], A[j] = A[j], A[i]
             i += 1
-
     A[i], A[hi] = A[hi], A[i]
     return i
 
-
 def nsmallest(m, A):
-    """
-    Возвращает `m` наименьших элементов массива A.
-    Функция реализована на основе Quicksort.
-    >>> nsmallest(3, [44, 64, 21, 86, 40, 46, 95])
-    [21, 40, 44]
-    """
+    quicksort(A, 0, len(A)-1)
+    return A[:m]
 
-    ...
+def quicksort(A, start, end):
+    if start < end:                       
+        pivot = partition(A, start, end)
+        quicksort(A, start, pivot - 1)
+        quicksort(A, pivot + 1, end)
+        
+print(nsmallest(3, [44, 64, 21, 86, 40, 46, 95, 73, 42, 20, 99, 15, 73]))
