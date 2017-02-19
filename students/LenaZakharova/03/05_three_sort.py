c8d8bb1e37368bf4c609afb012356f6e1bc1f91b
eszakharova <1583253@gmail.com>
2017-02-19 20:10:58 +0300

    p5_2

--- students/LenaZakharova/03/05_three_sort.py
+++ students/LenaZakharova/03/05_three_sort.py
@@ -1,28 +1,17 @@
 def partition(A, lo, hi):
-    # Возможно, в этой процедуре потребуются
-    # небольшие изменения
-
-    pivot = A[hi]
-    i = lo
-
-    for j in range(lo, hi):
-        # if A[j] <= pivot:
-        if (A[j] == 'R' and pivot == 'W') or (A[j] == 'W' and
-                pivot == 'B') or (A[j] == pivot) or (A[j] == 'R' and pivot == 'B'):
-            A[i], A[j] = A[j], A[i]
+    i = 0
+    while i <= hi:
+        if A[i] == 'W':  # оставить на месте
             i += 1
-
-    A[i], A[hi] = A[hi], A[i]
-    return i
-
-
-def pre_sort(li, first, last):
-    if first < last:
-        pivot = partition(li, first, last)
-        pre_sort(li, first, pivot - 1)
-        pre_sort(li, pivot + 1, last)
-    return li
-
+        elif A[i] == 'R':  # подвинуть в начало
+            A[lo], A[i] = A[i], A[lo]
+            lo += 1
+            i += 1
+        else:  # подвинуть в конец
+            A[hi], A[i] = A[i], A[hi]
+            # i += 1
+            hi -= 1
+    return A
 
 def three_sort(A):
     """
@@ -31,8 +20,8 @@ def three_sort(A):
     >>> three_sort(['B', 'B', 'W', 'R', 'B', 'R', 'R', 'W', 'W'])
     >>> ['R', 'R', 'R', 'W', 'W', 'W', 'B', 'B', 'B']
     """
-    return pre_sort(A, 0, len(A)-1)
+    return partition(A, 0, len(A)-1)
 
 # print(three_sort(['B', 'B', 'R', 'R', 'W', 'W', 'W', 'R', 'B']))
 # print(three_sort(['B', 'B', 'W', 'R', 'B', 'R', 'R', 'W', 'W']))
-# print(three_sort(['W', 'W', 'W', 'R', 'B', 'B', 'B', 'R', 'R']))
+# print(three_sort(['W', 'W', 'W', 'R', 'B', 'B', 'B', 'R', 'R', 'R']))
