113513c7746484292ae90fe2072f1452a69138e5
Valeriya <lerie438@gmail.com>
2017-02-20 00:15:43 +0300

    changed for-loop

--- students/Zelenkova_Lera/03/04_bisect_insertion.py
+++ students/Zelenkova_Lera/03/04_bisect_insertion.py
@@ -4,7 +4,10 @@ def insertion_sort(A):
     for j in range(1, len(A)):
         key = A[j]
         k = bisect(A, key, 0, j)
-        A = A[:k] + [key] + A[k:j] + A[j+1:]
+        for i in range(j, k-1, -1):
+            A[i] = A[i-1]
+        A[k] = key
+        #A = A[:k] + [key] + A[k:j] + A[j+1:]
 
     return A
 
