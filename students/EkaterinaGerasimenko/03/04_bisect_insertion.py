3c9a7dfd34493ab639cb5606b208d04becf9a8bc
katgerasimenko <katgerasimenko@gmail.com>
2017-02-19 15:36:04 +0400

    03-4 without slices

--- students/EkaterinaGerasimenko/03/04_bisect_insertion.py
+++ students/EkaterinaGerasimenko/03/04_bisect_insertion.py
@@ -3,7 +3,8 @@ from bisect import *
 def insertion_sort(A):
     for j in range(1, len(A)):
         key = A[j]
-        index = bisect(A[:j],key)
-        A[index+1:j+1] = A[index:j]
+        index = bisect(A,key,hi=j)
+        for i in range(j,index,-1):
+            A[i] = A[i-1]
         A[index] = key
     return A
