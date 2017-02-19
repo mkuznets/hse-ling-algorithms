a62b2b0b3751b4bb88008631a4368c5ed6311fd4
Polina Sonina <sarun4ik7@yandex.ru>
2017-02-19 22:35:44 +0300

    Update 05_three_sort.py

--- students/SoninaPolina/03/05_three_sort.py
+++ students/SoninaPolina/03/05_three_sort.py
@@ -1,24 +1,17 @@
-def partition(A, lo, hi):
-    # Возможно, в этой процедуре потребуются
-    # небольшие изменения
-
+def partition(A, lo, hi, L):
     pivot = A[hi]
     i = lo
-
     for j in range(lo, hi):
-        if A[j] <= pivot:
+        if A[j] != L:
             A[i], A[j] = A[j], A[i]
-            i += 1
-
+            i += 1            
     A[i], A[hi] = A[hi], A[i]
     return i
 
-
 def three_sort(A):
-    """
-    Делает in-place сортировку массива с элементами 'B', 'R', 'W'
-    по цветам флага Нидерландов. Время работы: O(n).
-    >>> three_sort(['B', 'B', 'W', 'R', 'B', 'R', 'R', 'W', 'W'])
-    >>> ['R', 'R', 'R', 'W', 'W', 'W', 'B', 'B', 'B']
-    """
-    pass
+    pivot = partition(A, 0, len(A)-1, 'B')
+    partition(A, 0, pivot, 'W')
+    partition(A, pivot+1, len(A)-1, 'B')
+    return A
+
+print(three_sort(['B', 'B', 'W', 'R', 'B', 'R', 'R', 'W', 'W', 'B', 'R', 'W', 'W', 'B', 'R', 'R']))
