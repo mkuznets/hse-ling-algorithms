eef082c9ca5f515ecc9d2286eb7d4facdc7c69c3
dariasuslova <dariasuslova483@gmail.com>
2017-06-14 20:57:34 +0300

    flatten

---
+++ students/DariaSuslova/flatten.py
@@ -0,0 +1,9 @@
+def flatten(root):
+    if root:
+        flatten(root.right)
+        if root.left:
+            flatten(root.left)
+            p = root.left
+            while p.right is not None:
+                p = p.right
+            p.right, root.right, root.left = root.right, root.left, None
