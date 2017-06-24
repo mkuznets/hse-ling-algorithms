b0032fd5becebbd994ff2d38e5b613fb37cf7bcb
Valeriya <lerie483@gmail.com>
2017-06-24 23:49:23 +0300

    changed file

--- problems/04/4hw_flatten.py
+++ problems/04/4hw_flatten.py
@@ -3,10 +3,13 @@ from binarytree import convert
 from binarytree import Node
 #
 root = Node(10)
-root.left = Node(6)
-root.right = Node(11)
-root.left.left = Node(5)
-root.left.right = Node(7)
+# root.left = Node(5)
+root.left = Node(8)
+root.right = Node(20)
+root.right.left = Node(15)
+root.right.right = Node(25)
+# root.right.left = Node(15)
+# root.right.right = Node(25)
 print(root)
 
 def flatten(node):
@@ -20,14 +23,20 @@ def flatten(node):
     flatten(node.left)
     flatten(node.right)
 
+    if node.right:
+        node.lastnode = node.right.lastnode
+        node.right = node.right.topnode
+
     if node.left:
         node.left.topnode.lastnode.right = node #делаем родителя node-a
         node.topnode = node.left.topnode
         if node.right:
+            # node.right.lastnode = None
             node.topnode.lastnode = node.right.lastnode
         else:
             node.topnode.lastnode = node
         node.left = None #стерли старую ссылку на левый узел
+
 
     return node.topnode
 
