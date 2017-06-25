e0e19065837bc66d33c06a0593c5094814b7488d
Valeriya <lerie483@gmail.com>
2017-06-25 11:50:20 +0300

    changed file

--- problems/04/4hw_flatten.py
+++ problems/04/4hw_flatten.py
@@ -2,42 +2,44 @@ import binarytree
 from binarytree import convert
 from binarytree import Node
 #
-root = Node(10)
-# root.left = Node(5)
-root.left = Node(8)
+root = Node(11)
+root.left = Node(9)
 root.right = Node(20)
 root.right.left = Node(15)
+root.left.right = Node(10)
 root.right.right = Node(25)
 # root.right.left = Node(15)
 # root.right.right = Node(25)
 print(root)
 
-def flatten(node):
-    if node is None: return
+def flatten(node, parent=None):
+    if node is None: return parent, parent
+    # if not node.right:
+    #     return node, node
 
-    if node.right is None:
-        node.lastnode = node
-    if node.left is None:
-        node.topnode = node
-
-    flatten(node.left)
-    flatten(node.right)
-
+    lastnode_L = flatten(node.left, node)[1]
     if node.right:
-        node.lastnode = node.right.lastnode
-        node.right = node.right.topnode
+        lastnode_R = flatten(node.right, node)[1]
+    else:
+        lastnode_R = node
 
     if node.left:
-        node.left.topnode.lastnode.right = node #делаем родителя node-a
-        node.topnode = node.left.topnode
-        if node.right:
-            # node.right.lastnode = None
-            node.topnode.lastnode = node.right.lastnode
-        else:
-            node.topnode.lastnode = node
-        node.left = None #стерли старую ссылку на левый узел
+        left = node.left
+        node.left = None
+        lastnode_L.right = node
+        if parent:
+            if node == parent.left:
+                parent.left = left
+            elif node == parent.right:
+                parent.right = left
+    else:
+        left = node
+    if parent:
+        return parent.left, lastnode_R
+    else:
+        return left, lastnode_R
 
 
-    return node.topnode
 
-print(flatten(root))
+root = flatten(root)[0]
+print(root)
